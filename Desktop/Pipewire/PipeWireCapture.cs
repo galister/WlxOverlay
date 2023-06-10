cd40bbca97fbdbb4877e15cca1a17184069f92be
galister <22305755+galister@users.noreply.github.com>
2023-06-10 16:10:20 +0200

    pw frame logging

--- Desktop/Pipewire/PipeWireCapture.cs
+++ Desktop/Pipewire/PipeWireCapture.cs
@@ -190,6 +190,7 @@ public class PipeWireCapture : IDisposable
             {
                 case spa_data_type.SPA_DATA_DmaBuf:
                     {
+                        Console.WriteLine("Got SPA_DATA_DmaBuf");
                         var planes = pb->n_datas;
 
                         var format = FromSpaFormats[info->raw.format];
@@ -221,6 +222,7 @@ public class PipeWireCapture : IDisposable
                     }
                 case spa_data_type.SPA_DATA_MemFd:
                     {
+                        Console.WriteLine("Got SPA_DATA_MemFd");
                         _attribs[0] = (nint)spa_data_type.SPA_DATA_MemFd;
                         _attribs[1] = (nint)pb->datas[0].fd;
                         _attribs[2] = (nint)pb->datas[0].chunk->size;
@@ -229,10 +231,16 @@ public class PipeWireCapture : IDisposable
                     }
                 case spa_data_type.SPA_DATA_MemPtr:
                     {
+                        Console.WriteLine("Got SPA_DATA_MemPtr");
                         _attribs[0] = (nint)spa_data_type.SPA_DATA_MemPtr;
                         _attribs[1] = pb->datas[0].data;
                         break;
                     }
+                default:
+                {
+                    Console.WriteLine($"Unsupported buffer type: {pb->datas[0].type}");
+                    break;
+                }
             }
         }
     }
